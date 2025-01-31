abf343f2fb0744144cb23a5bb18aac691e1d6ed2
Ye Zhenrong <3471626272@qq.com>
2025-01-31 17:03:06 +0800

    feat(mybatis-plus): configure pagination and mapper scanning
    - Add MyBatis-Plus JSqlParser dependency
    - Create MybatisPlusConfig with pagination interceptor
    - Move @MapperScan to configuration class
    - Remove @MapperScan from main application class

--- src/main/java/com/demo/spring_demo/SpringDemoApplication.java
+++ src/main/java/com/demo/spring_demo/SpringDemoApplication.java
@@ -1,11 +1,9 @@
 package com.demo.spring_demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.demo.spring_demo.mapper")
 public class SpringDemoApplication {
 
     public static void main(String[] args) {
